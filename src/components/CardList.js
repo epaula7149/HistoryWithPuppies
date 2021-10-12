69392e59003ef57b267ddde1178dceba9bf5921d
epaula100721 <elwynanderson@elwyns-iMac.local>
2021-10-12 16:08:52 -0500

    working project

---
+++ src/components/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+let id = 0;
+const CardList = ({robots}) => { 
+	return (
+		<div>
+		{
+			robots.map((user, i) => {
+				return (
+					<Card 
+					key={id+i} 
+					id={robots[i].id} 
+					name={robots[i].name} 
+					email={robots[i].email}
+					/>
+				);
+			})
+		}
+		</div>
+	);
+}
+
+export default CardList;
