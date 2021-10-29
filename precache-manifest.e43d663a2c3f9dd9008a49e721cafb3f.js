19a0bd93d5f7a1745dc594d732f08fd8771f6499
epaula100721 <elwynanderson@elwyns-iMac.local>
2021-10-29 15:01:37 -0500

    Updates

---
+++ precache-manifest.e43d663a2c3f9dd9008a49e721cafb3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbc016c1d9bff057068bf223fb7931c",
+    "url": "/HistoryWithPuppies/index.html"
+  },
+  {
+    "revision": "476c9c4ee298dde5673d",
+    "url": "/HistoryWithPuppies/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "73550f40117bfb244b76",
+    "url": "/HistoryWithPuppies/static/css/main.bef57228.chunk.css"
+  },
+  {
+    "revision": "476c9c4ee298dde5673d",
+    "url": "/HistoryWithPuppies/static/js/2.affc2760.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/HistoryWithPuppies/static/js/2.affc2760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73550f40117bfb244b76",
+    "url": "/HistoryWithPuppies/static/js/main.79164699.chunk.js"
+  },
+  {
+    "revision": "41e945c58a59f410e0a0",
+    "url": "/HistoryWithPuppies/static/js/runtime-main.0ca65f01.js"
+  }
+]);
